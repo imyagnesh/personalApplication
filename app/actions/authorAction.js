8c51d0cd3b2c1a9b0713351d0d1b771504290081
Yagnesh Modh <yagnesh.modh@gmail.com>
2016-08-31 20:54:52 +0530

    update formatting

--- app/actions/authorAction.js
+++ app/actions/authorAction.js
@@ -1,18 +1,18 @@
 import * as types from './actionTypes';
-import {beginAjaxCall} from './ajaxStatusAction'; 
+import { beginAjaxCall } from './ajaxStatusAction';
 import authorApi from './../api/mockAuthorApi';
 
-export function loadAuthorsSuccess(authors)  {
-    return  { type: types.LOAD_AUTHORS_SUCCESS, authors };
-}   
+export function loadAuthorsSuccess(authors) {
+  return { type: types.LOAD_AUTHORS_SUCCESS, authors };
+}
 
 export function loadAuthors() {
-    return function(dispatch){
-        dispatch(beginAjaxCall());
-        return authorApi.getAllAuthors().then(authors => {
-            dispatch(loadAuthorsSuccess(authors));
-        }).catch(error => {
-            throw(error);
-        });
-    };
+  return function getAllAuthors(dispatch) {
+    dispatch(beginAjaxCall());
+    return authorApi.getAllAuthors().then(authors => {
+      dispatch(loadAuthorsSuccess(authors));
+    }).catch(error => {
+      throw (error);
+    });
+  };
 }
