dc6dc068026fa57216c8b848a871ea1075bea187
Yagnesh Modh <yagnesh.modh@gmail.com>
2016-09-22 15:17:23 +0530

    change server

--- server.js
+++ server.js
@@ -46,10 +46,6 @@ if (isDeveloping) {
   });
 }
 
-app.listen(port, '0.0.0.0', function onStart(err) {
-  if (err) {
-    console.log(err);
-  }
-  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-  open(`http://localhost:${port}`);
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
