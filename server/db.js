db5cc2889767a7ba1f20ad5247c643c6664ff92e
Yagnesh Modh <yagnesh.modh@gmail.com>
2016-10-22 13:57:19 +0530

    change db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://localhost/personal_app';
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
 // let mongoURLLabel = '';
 
 
